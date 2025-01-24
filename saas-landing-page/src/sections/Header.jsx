c2cf533e5cdc0f56be0e59ee7dccbcbf7b07527e
addod <addod@Davids-MacBook-Pro.local>
2025-01-24 08:51:05 -0600

    Inital testing with custom classes

---
+++ saas-landing-page/src/sections/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<header>
+			<div>
+				<a>
+					<img src='/images/xora.svg' width={115} height={55} alt='logo' />
+				</a>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
