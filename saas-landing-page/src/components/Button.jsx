eb346cb77a350ff0f47f4c03246bc5e45e0e4abf
addod <addod@Davids-MacBook-Pro.local>
2025-01-30 11:41:41 -0600

    Renamed Index.js to Index.jsx and added to this file

--- saas-landing-page/src/components/Button.jsx
+++ saas-landing-page/src/components/Button.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { Marker } from "./Marker.jsx";
+import Marker from "./Marker.jsx";
 
 const Button = ({
 	icon,
@@ -11,8 +11,8 @@ const Button = ({
 }) => {
 	const Inner = () => (
 		<>
-			<span>
-				<span>
+			<span className='relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden'>
+				<span className='absolute -left-[1px]'>
 					<Marker markerFill={markerFill} />
 				</span>
 				{icon && (
@@ -22,14 +22,18 @@ const Button = ({
 						className='size-10 mr-5 object-contain z-10'
 					/>
 				)}
-				<span>{children}</span>
+				<span className='relative z-2 font-poppins base-bold text-p1 uppercase'>
+					{children}
+				</span>
 			</span>
+
+			<span className='glow-before glow-after' />
 		</>
 	);
 	return href ? (
 		<a
 			className={clsx(
-				"relative, p-0.5 g5 rounded-2xl shadow-500 group",
+				"relative p-0.5 g5 rounded-2xl shadow-500 group",
 				containerClassName
 			)}
 			href={href}
@@ -39,7 +43,7 @@ const Button = ({
 	) : (
 		<button
 			className={clsx(
-				"relative, p-0.5 g5 rounded-2xl shadow-500 group",
+				"relative p-0.5 g5 rounded-2xl shadow-500 group",
 				containerClassName
 			)}
 			onClick={onClick}
--- saas-landing-page/src/sections/Header.jsx
+++ saas-landing-page/src/sections/Header.jsx
@@ -20,7 +20,9 @@ const Header = () => {
 				<div
 					className={clsx(
 						"w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
-						isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+						isOpen
+							? "transition-opacity duration-500 max-lg:opacity-100"
+							: "transition-opacity duration-500 max-lg:pointer-events-none"
 					)}
 				>
 					<div className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before'>
