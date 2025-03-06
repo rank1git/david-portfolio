3fb431904c901f7f2761e7b89292f304d677b6a4
addod <addod@Davids-MacBook-Pro.local>
2025-03-06 16:25:06 -0600

    Downloads and testimonials section

--- saas-landing-page/src/App.jsx
+++ saas-landing-page/src/App.jsx
@@ -1,7 +1,10 @@
+import Downloads from "./sections/Downloads";
+import Faq from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
 	return (
@@ -10,6 +13,9 @@ const App = () => {
 			<Hero />
 			<Features />
 			<Pricing />
+			<Faq />
+			<Testimonials />
+			<Downloads />
 		</main>
 	);
 };
